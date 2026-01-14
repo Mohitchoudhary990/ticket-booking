28624dbb85aa58ca81630b6bb5addc6d75c48399
Mohitchoudhary990 <mohit233choudhary@gmail.com>
2026-01-14 22:47:04 +0530

    Add booking cancellation with transaction and seat rollback

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const eventRoutes = require("./routes/eventRoutes");
+const authRoutes = require("./routes/authRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/events", eventRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
 app.get("/", (req, res) => {
   res.send("Ticket Booking API is running");
 });
